59190e3c534fdc8d1c925b3ac18660cf1a8a0f1f
MrDaniLLka <danilamakotin@gmail.com>
2021-10-31 16:23:11 +0300

    DataFrame

---
+++ DataFrame.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[8]:
+
+
+df = pd.read_csv('ru_profane_words.txt', header=None)
+
+
+# In[10]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
