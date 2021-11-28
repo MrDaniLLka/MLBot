d6e26eed58a1af37fe26569a6921e8ae8f10b512
MrDaniLLka <danilamakotin@gmail.com>
2021-11-28 20:55:08 +0300

    Bot+

--- BotForVK.py
+++ BotForVK.py
@@ -5,6 +5,8 @@ from config import tkn
 session = vk_api.VkApi(token = tkn)
 longpoll =  VkLongPoll(session)
 
+clf = pickle.load(open(os.path.join('model.pkl'), 'rb'))
+
 print("starting bot")
 def Send(id, text):
     session.method('messages.send', {'chat_id' : id, 'message' : text, 'random_id' : 0})
@@ -13,4 +15,5 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.to_me:
         id = event.chat_id
         message = event.text.lower()
-        Send(id, message)
+        if clf.predict([message]) == [1]:
+            Send(id, 'Не материтесь')
