1bdb5918e5004fe0abab926535c369abb21ec7aa
strdnrm <93453304+strdnrm@users.noreply.github.com>
2021-11-30 01:37:37 +0300

    Added a model to the bot and added a reference to the user

--- BotForVK.py
+++ BotForVK.py
@@ -1,9 +1,13 @@
 import vk_api 
 from vk_api.longpoll import VkLongPoll, VkEventType
 from config import tkn
+import pickle
+import os
 
 session = vk_api.VkApi(token = tkn)
 longpoll =  VkLongPoll(session)
+
+vk_session = session.get_api()
 
 clf = pickle.load(open(os.path.join('model.pkl'), 'rb'))
 
@@ -15,5 +19,6 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.to_me:
         id = event.chat_id
         message = event.text.lower()
-        if clf.predict([message]) == [1]:
-            Send(id, 'Не материтесь')
+        name = event.chat_id
+        if clf.predict([message]) == [1]:            
+            Send(id, f'https://vk.com/id{event.user_id}, не ругайтесь')
